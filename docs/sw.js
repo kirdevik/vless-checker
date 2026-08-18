3bdb8fdfc8de97aa0d32336a9e1dabe8d9949cf7
kirdevik <kirdevik@gmail.com>
2026-08-18 10:48:23 +0300

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,1 +1,5 @@
-
+const CACHE_NAME = 'vless-checker-v1';
+const urlsToCache = ['/vless-checker/','/vless-checker/index.html','/vless-checker/styles.css','/vless-checker/script.js','/vless-checker/manifest.json'];
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))); });
+self.addEventListener('fetch', event => { event.respondWith(caches.match(event.request).then(response => response || fetch(event.request))); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(cacheNames => Promise.all(cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))))); });
